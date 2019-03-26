cacc1e05a381d517e32f429aca41be59d18944a1
Max <maxime.belbeoch5@gmail.com>
2019-03-26 10:07:57 +0100

    création 404, premier test api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -6,11 +6,29 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use App\Avatar;
+use App\User;
 
 class ApiController extends Controller
 {
     public function display($userName = ''){
 
-        return 'test : ' . $userName;
+        if($userName){
+
+            $dataUser = User::whereName($userName)->get();
+
+            if($dataUser != ''){
+
+                return $dataUser;
+            }
+            else{
+
+                return 'aucune icône ne correspond à ce nom de compte ...';
+            }
+        }
+        else{
+
+            return 'false';
+        }
     }
 }
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page not found - 404</title>
+</head>
+<body>
+ 
+ 
+La page que vous cherchez est introuvable
+ 
+</body>
+</html>
