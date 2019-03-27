03e3871810a7a91e3822cc68c12c449e3c02487c
Max <maxime.belbeoch5@gmail.com>
2019-03-27 09:22:09 +0100

    maj controller upload

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,39 +29,4 @@ class HomeController extends Controller
     {
         return view('home');
     }
-
-    public function imgUpload(Request $request){
-
-        // on vérifie les champs saisis par l'utilisateur
-        $this->validate( $request, 
-            [ 
-            'img' => 'required|mimes:jpeg,jpg,png',
-            'email' => 'required|email'
-            ],
-            [
-            'img.required' => 'Vous devez sélectionner une image',
-            'img.mimes' => 'Formats d\'image autorisés : JPEG, JPG ou PNG',
-            'email.required' => 'Vous devez saisir un e-mail'
-            ]);
-
-        // on renomme l'image uploadée et on l'enregistre dans public/images
-        $imageName = time().'.'.request()->img->getClientOriginalExtension();
-        request()->img->move(public_path('images'), $imageName);
-
-        $mail = request()->email;
-        $userId = \Auth::user()->id;
-
-        $data = [
-            'email' => $mail,
-            'picture' => 'images/' . $imageName,
-            'users_id' => $userId
-        ];
-
-        // On ajoute les données saisies ainsi que l'url de l'image dans la table Avatar
-        Avatar::insert($data);
-
-        // On retourne sur la page précédente avec un message de confirmation
-        return back()->with('success','Image envoyée avec succès !');
-
-    }
 }
--- resources/views/uploadImg.blade.php
+++ resources/views/uploadImg.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Espace utilisateur</div>
+                <div class="card-header">Espace utilisateur - Ajouter un avatar</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -17,9 +17,23 @@
                     @if ($message = Session::get('success'))
                         <div class="alert alert-success alert-block">
                             <button type="button" class="close" data-dismiss="alert">×</button>
-                            <strong>{{ $message }}</strong>
+                                <strong>{{ $message }}</strong>
                         </div>
                     @endif
+
+                    {!! Form::open(['route' => 'formRoute', 'files' => true]) !!}
+    
+                        Adresse e-mail : 
+                        {!! Form::email('email') !!}<br><br>
+                        Image à associer : 
+                        {!! Form::file('img') !!}<br><br>
+                        {!! Form::submit('envoyer') !!}
+
+                    {!! Form::close() !!}
+
+                    @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                    @endforeach
                 </div>
             </div>
         </div>
